dee8ff0d50f160a77c469fa366e967e05550a4b0
Derek Chang <derekchangc00@gmail.com>
2024-12-26 19:14:09 -0800

    Fix HTML entities in About page for proper rendering of apostrophes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,20 +5,20 @@ export default function About() {
         <h1 className="text-4xl font-bold mb-8">About Me</h1>
         <div className="max-w-8xl">
           <p>
-            Hi, I'm Derek. My profession is developing software @ Veeva. Outside
-            of working hours, I keep myself busy with home cooking, recreational
-            sports, watching movies, and much much more.
+            Hi, I&apos;m Derek. My profession is developing software @ Veeva.
+            Outside of working hours, I keep myself busy with home cooking,
+            recreational sports, watching movies, and much much more.
           </p>
           <br />
           <h2 className="text-2xl font-bold mb-4">
-            What's that point of this website?
+            What&apos;s that point of this website?
           </h2>
           <p>
-            I'm not too sure at the moment. My best guess is that it will be a
-            place for old friends, new friends, recruiters, and complete
-            strangers to know more about me and what I'm up. Kind of like social
-            media but without the common side effects I notice I get when I post
-            there.
+            I&apos;m not too sure at the moment. My best guess is that it will
+            be a place for old friends, new friends, recruiters, and complete
+            strangers to know more about me and what I&apos;m up. Kind of like
+            social media but without the common side effects I notice I get when
+            I post there.
           </p>
         </div>
       </main>

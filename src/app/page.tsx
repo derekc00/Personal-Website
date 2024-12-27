590745491f9135298f93080e62532089849780e3
Derek Chang <derekchangc00@gmail.com>
2024-12-26 18:10:05 -0800

    Add spanning video to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,37 @@
+import { Suspense } from "react";
+import { list } from "@vercel/blob";
+
 export default function Home() {
   return (
     <div className="relative">
       <div className="h-screen w-full">
-        <img
-          src="/home.jpg"
-          alt="Hero image"
-          className="w-full h-full object-cover"
-        />
+        <Suspense fallback={<div>Loading...</div>}>
+          <VideoComponent fileName="derek-in-the-park-FVGcatY5vGoJYmtDg1HDIA207UwXMj.mp4" />
+        </Suspense>
       </div>
       {/* Your other content */}
     </div>
+  );
+}
+
+async function VideoComponent({ fileName }: { fileName: string }) {
+  const { blobs } = await list({
+    prefix: fileName,
+    limit: 1,
+  });
+  const { url } = blobs[0];
+
+  return (
+    <video
+      autoPlay
+      loop
+      muted
+      playsInline
+      aria-label="Background video"
+      className="w-full h-full object-cover absolute top-0 left-0"
+    >
+      <source src={url} type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
   );
 }

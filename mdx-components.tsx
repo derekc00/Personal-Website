cd3cb4b40a8d89143e1336228b2e49fb4ecb3fe2
Derek Chang <derekchangc00@gmail.com>
2025-04-12 16:35:10 -0700

    Add syntax highlighting for code blocks using sugar-high, update MDX components, and include a new coding example blog post

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,9 +1,6 @@
 import * as React from "react";
 import type { MDXComponents } from "mdx/types";
-
-// todo: might need to add these back in
-// import { Callout } from "~/components/mdx/callout";
-// import { Codeblock } from "~/components/mdx/code-block";
+import { highlight } from "sugar-high";
 
 // This file is required to use MDX in `app` directory.
 export function useMDXComponents(components: MDXComponents): MDXComponents {
@@ -11,6 +8,35 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     wrapper: ({ children }) => (
       <div className="prose dark:prose-dark max-w-none">{children}</div>
     ),
+    code: (props: any) => {
+      // NOTE: I don't know how to make this type correct. Had to put any for now.
+      const { className, children } = props;
+      const language = className?.replace("language-", "");
+
+      if (className) {
+        // Code block with language
+        const highlightedCode = highlight(String(children), language);
+        return (
+          <div className="relative my-6">
+            {language && (
+              <div className="absolute right-2 top-2 text-xs text-gray-500 dark:text-gray-400 font-mono">
+                {language}
+              </div>
+            )}
+            <pre className="p-4 rounded-lg overflow-x-auto bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-gray-200">
+              <code dangerouslySetInnerHTML={{ __html: highlightedCode }} />
+            </pre>
+          </div>
+        );
+      }
+
+      // Inline code
+      return (
+        <code className="font-mono text-sm px-1 py-0.5 rounded bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-gray-200">
+          {children}
+        </code>
+      );
+    },
     ...components,
   };
 }
